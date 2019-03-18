803a58d6821579f815d00a74ef2774fc309a2cd9
i.selivanov <i.selivanov@localhost.localdomain>
2019-03-18 22:59:45 +0300

    microfix

--- MRI_images/handle_predict.py
+++ MRI_images/handle_predict.py
@@ -1,6 +1,6 @@
 import tables
 import predict_h5
-from training import load_old_model
+from unet3d.training import load_old_model
 
 model = load_old_model('tumor_segmentation_model.h5')
 data_file = tables.open_file('data/h5/ozerki.h5', "r")
