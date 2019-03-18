a3fcddefa7de30c8c0a87cfb7a404ce845bce84f
i.selivanov <i.selivanov@localhost.localdomain>
2019-03-18 22:14:20 +0300

    Add hadle_convert_ozerki

---
+++ MRI_images/handle_convert.py
@@ -0,0 +1,12 @@
+import os
+import convert_to_h5
+
+raw_data_list = []
+nii_root_path = "data/original/ozerki/"
+for i in range(1, 9):
+    raw_data = os.path.join(nii_root_path, f"G{i}.nii.gz")
+    raw_data_list.append((raw_data,))
+
+convert_to_h5.write_data_to_file(raw_data_list, 'data/h5/ozerki.h5', (144, 144, 144))
+
+
