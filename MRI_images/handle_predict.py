0377b8a319140cc4654c803e54de1192bf3d704f
i.selivanov <i.selivanov@localhost.localdomain>
2019-03-18 23:29:01 +0300

    microfix

--- MRI_images/handle_predict.py
+++ MRI_images/handle_predict.py
@@ -6,5 +6,5 @@ model = load_old_model('tumor_segmentation_model.h5')
 data_file = tables.open_file('data/h5/ozerki.h5', "r")
 
 for i in range(9):
-    with open(f"result{i}", "r") as f:
+    with open(f"data/out/result{i}", "w") as f:
         print(predict_h5.run_validation_case(i, 'data/out', model, data_file, ["t1", "t1ce", "flair", "t2"]), file=f)
