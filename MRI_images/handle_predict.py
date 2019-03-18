712dd520522b3981e59a6056b79276209f181a5f
i.selivanov <i.selivanov@localhost.localdomain>
2019-03-18 23:27:02 +0300

    handle_predict теперь дает результат по всем G из ozerki.h5

--- MRI_images/handle_predict.py
+++ MRI_images/handle_predict.py
@@ -5,4 +5,6 @@ from unet3d.training import load_old_model
 model = load_old_model('tumor_segmentation_model.h5')
 data_file = tables.open_file('data/h5/ozerki.h5', "r")
 
-predict_h5.run_validation_case(0, 'data/out', model, data_file, ["t1", "t1ce", "flair", "t2"])
+for i in range(9):
+    with open(f"result{i}", "r") as f:
+        print(predict_h5.run_validation_case(i, 'data/out', model, data_file, ["t1", "t1ce", "flair", "t2"]), file=f)
