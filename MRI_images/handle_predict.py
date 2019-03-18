f024cf11eea23380d90169eecf41af65088595df
i.selivanov <i.selivanov@localhost.localdomain>
2019-03-18 22:54:14 +0300

    add handle for predict

---
+++ MRI_images/handle_predict.py
@@ -0,0 +1,8 @@
+import tables
+import predict_h5
+from training import load_old_model
+
+model = load_old_model('tumor_segmentation_model.h5')
+data_file = tables.open_file('data/h5/ozerki.h5', "r")
+
+predict_h5.run_validation_case(0, 'data/out', model, data_file, ["t1", "t1ce", "flair", "t2"])
