6a79c356a9b5fa757459bd0d8195c65286d1b5f6
i.selivanov <i.selivanov@localhost.localdomain>
2019-04-05 19:28:11 +0300

    Change model to isensee_2017_model.h5

--- MRI_images/handle_predict.py
+++ MRI_images/handle_predict.py
@@ -2,7 +2,7 @@ import tables
 import predict_h5
 from unet3d.training import load_old_model
 
-model = load_old_model('tumor_segmentation_model.h5')
+model = load_old_model('isensee_2017_model.h5')
 data_file = tables.open_file('data/h5/ozerki.h5', "r")
 
 for i in range(9):
