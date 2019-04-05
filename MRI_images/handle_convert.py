f35998c9e1063d1f2a467737934b069ff8476aa6
i.selivanov <i.selivanov@localhost.localdomain>
2019-04-05 18:08:09 +0300

    Add black_stub

--- MRI_images/handle_convert.py
+++ MRI_images/handle_convert.py
@@ -5,7 +5,7 @@ raw_data_list = []
 nii_root_path = "data/original/ozerki/"
 for i in range(1, 10):
     raw_data = os.path.join(nii_root_path, f"G{i}.nii.gz")
-    black_stub = os.path.join(nii_root_path, "")
-    raw_data_list.append((raw_data,))
+    black_stub = os.path.join(nii_root_path, "black.nii.gz")
+    raw_data_list.append((raw_data, black_stub, black_stub, black_stub))
 
 convert_to_h5.write_data_to_file(raw_data_list, 'data/h5/ozerki.h5', (144, 144, 144))
